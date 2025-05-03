fb3191bf125e47ab80bcf38dcfb09e0d0a1ecab1
matinsekhavat <matinhyphen@gmail.com>
2025-05-03 23:49:30 +0330

    feat: Export table schemas for orders, products, users, and their relations

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,4 @@
+export { OrderTable } from "./schemas/orders";
+export { ProductTable } from "./schemas/products";
+export { UserTable } from "./schemas/users";
+export { OrderToProductsTable } from "./schemas/orderToProducts";
